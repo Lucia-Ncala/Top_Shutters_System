65539aae13e4265f11b577efdeac1e01b7e8b2c8
Lucia Ncala <86241666+Lucia-Ncala@users.noreply.github.com>
2025-05-29 18:46:41 +0200

    linked the form_function.js to the application_form.html, and added script to change navbar color on scroll

---
+++ Functions/form_function.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+// Change navbar color on scroll
+window.addEventListener("scroll", function () {
+    const header = document.getElementById("header");
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+});
